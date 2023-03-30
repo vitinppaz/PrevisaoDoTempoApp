2f048243597946951429a0da40ed2a4de6a25be5
vitinppaz <pixelpaz2004@gmail.com>
2023-03-30 12:52:28 -0300

    arrumando API key

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
+    const APIKey = 'cf601ba051374ccede00f91c006ed9b9';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '') 
